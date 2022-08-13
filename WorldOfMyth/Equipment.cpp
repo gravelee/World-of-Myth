d52610651313f5b044cd69a0aee7bdbe6c4b59a2
gravelee <86350945+gravelee@users.noreply.github.com>
2022-08-13 19:45:37 +0300

    Update Equipment.cpp

--- WorldOfMyth/Equipment.cpp
+++ WorldOfMyth/Equipment.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "Equipment.h"
 #include "Item.h"
+
+using std::to_string;
+using std::vector;
+using std::string;
+using std::cout;
+using std::move;
 
 Equipment::Equipment(
 
@@ -28,17 +36,23 @@ Equipment::Equipment(
         natuRes{natuRes}, shadoRes{shadoRes}, cDurability{cDurability}, 
         iDurability{iDurability}, iClass{iClass}, setInfo{setInfo}{
     
-    this->equips = std::move(equips);
-    this->chances = std::move(chances);
+    this->equips = move(equips);
+    this->chances = move(chances);
 }
 
-void printItem(){
+void Equipment::printItem() const{
     
-    /*
+    /*  Prints
     
     string name, short int itemLvl, bool questItem, bool bind, unsigned short unique, 
     unsigned int duration, bool beginsAQuest, bool opens, string gearSlot,
- 
+  
+    */
+    printPart1();
+    
+    
+    /*  Prints
+    
     string gearType, unsigned short minDmg, unsigned short maxDmg,
     float speed, float dps, unsigned short armor, unsigned short str,
     unsigned short agi, unsigned short sta, unsigned short inte, unsigned short sprt,
@@ -46,46 +60,108 @@ void printItem(){
     unsigned short holyRes, unsigned short natuRes, unsigned short shadoRes,
     unsigned short cDurability, unsigned short iDurability, string iClass,
     
+    */
+    printPart1_5();
+    
+    
+    /*  Prints
+    
     unsigned short lvlReq, string skillTypeReq, unsigned short skillPointsReq, 
     string repTypeReq, unsigned short repPointsReq, string use,
     
+    */
+    printPart2();
+    
+    
+    /*  Prints
+    
     vector<string>&& equips, vector<string>&& chances, 
+    
+    */
+    printPart2_5();
+    
+    
+    /*  Prints
     
     string summary, unsigned short cStack, unsigned short iStack, 
     
+    */
+    printPart3();
+    
+    
+    /*  Prints
+    
     string setInfo, 
     
-    unsigned int sellPrice
+    */
+    printPart3_5();
     
     
-    std::cout<<"\n"<<Item::name
-        <<"\nItem Level "<<itemLvl
-        <<(questItem?"\nQuest Item":"")
-        <<(bind?"\nBinds when picked up":"")
-        <<((unique!=0)?"\nUnique" 
-            + ((unique!=1)?" (" + std::to_string(unique) + ")":"")   :"")   // it is updated further from vanilla
-        <<(duration?"\nDuration: " + std::to_string(duration):"")   // What duration?
-        <<(beginsAQuest?"\nThis item begins a quest":"")
-        <<(opens?"\n<Right click to open>":"")
-        <<((gearSlot!="None")?"\n" + gearSlot:"")
-        <<((lvlReq!=1)?"\nRequires Level " + std::to_string(lvlReq):"")
-        <<((skillTypeReq!="None")?"\nRequires " 
-            + skillTypeReq + "(" + std::to_string(skillPointsReq) + ")":"")
-        <<((repTypeReq!="None")?"\nRequires " 
-            + repTypeReq + "(" + std::to_string(repPointsReq) + ")":"")
-        <<((use!="None")?"\n" + morphString(use,false) + "\n":"")
-        <<((summary!="None")?"\n" + morphString(summary,true):"")
-        <<((cStack>1)?"\nMax Stack: " + std::to_string(cStack):"")
-        <<((iStack>1)?"\nMax Stack: " + std::to_string(iStack):"")
-        <<((sellPrice!=0)?"\nSell Price: " + std::to_string(sellPrice):"")
-        <<"\n\n";
+    /*  Prints
     
-     //*/
+    unsigned int sellPrice.
     
-    //(*this).printPart1();
+    */
+    printPart4();
     
-    //std::cout<<
+}
+
+void Equipment::printPart1_5() const{
     
+    /*  Prints
+    
+    string gearType, unsigned short minDmg, unsigned short maxDmg,
+    float speed, float dps, unsigned short armor, ***unsigned short block***, unsigned short str,
+    unsigned short agi, unsigned short sta, unsigned short inte, unsigned short sprt,
+    unsigned short arcRes, unsigned short fireRes, unsigned short frostRes,
+    unsigned short holyRes, unsigned short natuRes, unsigned short shadoRes,
+    unsigned short cDurability, unsigned short iDurability, string iClass,
+    
+    */
+    
+        //  formate everything better with stream flags
+    
+    cout<<(gearType!="None"?"\t\t\t\t" + gearType:"")
+        <<(minDmg!=0?"\n" + to_string(minDmg):"")
+        <<(maxDmg!=0?"-" + to_string(maxDmg) + " Damage":"")
+        <<(speed!=0.0?"\tSpeed " + to_string(speed):"")
+        <<(dps!=0.0?"\n(" + to_string(dps) + " damage per second)":"")
+        <<(armor!=0?"\n" + to_string(armor) + " Armor":"")
+        
+        //  add block!!
+        
+        <<(str!=0?"\n+" + to_string(str) + " Strength":"")
+        <<(agi!=0?"\n+" + to_string(agi) + " Agility":"")
+        <<(sta!=0?"\n+" + to_string(sta) + " Stamina":"")
+        <<(inte!=0?"\n+" + to_string(inte) + " Intelect":"")
+        <<(sprt!=0?"\n+" + to_string(sprt) + " Spirit":"")
+        <<(arcRes!=0?"\n+" + to_string(arcRes) + " Arcane Resistance":"")
+        <<(fireRes!=0?"\n+" + to_string(fireRes) + " Fire Resistance":"")
+        <<(frostRes!=0?"\n+" + to_string(frostRes) + " Frost Resistance":"")
+        <<(holyRes!=0?"\n+" + to_string(holyRes) + " Holy Resistance":"")
+        <<(natuRes!=0?"\n+" + to_string(natuRes) + " Natural Resistance":"")
+        <<(shadoRes!=0?"\n+" + to_string(shadoRes) + " Shadow Resistance":"")
+        <<(cDurability!=0?"\nDurability " + to_string(cDurability):"")
+        <<(iDurability!=0?" / " + to_string(iDurability):"")
+        <<(iClass!="None"?"\nClasses: " + iClass:"");
+        
+        //  Make iClass a vector<string> !!!
+        
+}
+
+void Equipment::printPart2_5() const{
+    
+    for( string str: equips)
+        cout<<"\n" + morphString(str,false);
+    
+    for( string str: chances)
+        cout<<"\n" + morphString(str,false);
+    
+}
+
+void Equipment::printPart3_5() const{
+    
+    cout<<((setInfo!="None")?"\n\n" + setInfo:"");
 }
 
 Equipment::~Equipment(){
@@ -93,4 +169,3 @@ Equipment::~Equipment(){
     equips.clear();
     chances.clear();
 }
-
